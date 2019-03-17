80fc923bcec0313d9b965784e9935f29df482ae4
lyutok <tokarlyudmila@gmail.com>
2019-03-17 12:19:44 +0200

    commit1

--- Solution_G2/ChildClass.cs
+++ Solution_G2/ChildClass.cs
@@ -4,7 +4,7 @@ namespace Solution_G2
 {
     public class ChildClass : ParentClass 
     {
-        public int childInt = 67;
+        public int childInt = 68;
         public override void TestMethod()
         {
             Console.WriteLine("Child class");
