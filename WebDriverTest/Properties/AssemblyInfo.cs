239800e00dfeb85b7971eebeb18ab7288d5d73ca
lyutok <tokarlyudmila@gmail.com>
2019-03-17 15:45:12 +0200

    new

---
+++ WebDriverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDriverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDriverTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("263ef5fe-6860-4194-b8fb-05cd69a5a5b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebDriverTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace WebDriverTest
+{
+    
+    public class UnitTest1
+    {
+        
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+        }
+    }
+}
