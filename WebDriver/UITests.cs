779fb0645b69d41ed9d67c4690eaf01deecd822b
lyutok <tokarlyudmila@gmail.com>
2019-03-17 14:02:02 +0200

    add selenium WebDriver

--- WebDriver/UITests.cs
+++ WebDriver/UITests.cs
@@ -4,9 +4,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace WebDriver
 {
     [TestClass]
-    public class UnitTest1
+    public class UITests
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
         }
